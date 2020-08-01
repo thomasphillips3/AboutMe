34fdf315f7008109285d9d52f10787e97fafc5a4
Thomas Phillips <thomasphillips3@gmail.com>
2020-08-01 13:16:55 -0700

    add nickname button

--- app/src/main/java/com/thomasphillips3/aboutme/MainActivity.kt
+++ app/src/main/java/com/thomasphillips3/aboutme/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.thomasphillips3.aboutme
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun updateNickname(view: View) {
+        nicknameText.text = nicknameEdit.text
+        nicknameEdit.text.clear()
+        nicknameText.visibility = View.VISIBLE
     }
 }
