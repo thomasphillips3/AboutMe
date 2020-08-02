bf6d5f7c88335e48c719de9d66874fa554ab8808
Thomas Phillips <thomasphillips3@gmail.com>
2020-08-01 18:56:49 -0700

    use data binding

--- app/src/main/java/com/thomasphillips3/aboutme/MainActivity.kt
+++ app/src/main/java/com/thomasphillips3/aboutme/MainActivity.kt
@@ -1,19 +1,37 @@
 package com.thomasphillips3.aboutme
 
+import android.content.Context
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.databinding.DataBindingUtil
+import com.thomasphillips3.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val myName = MyName("Thomas Phillips")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.myName = myName
+        binding.button.setOnClickListener{
+            updateNickname(it)
+        }
     }
 
     fun updateNickname(view: View) {
-        nicknameText.text = nicknameEdit.text
-        nicknameEdit.text.clear()
-        nicknameText.visibility = View.VISIBLE
+        binding.apply {
+            myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll()
+            nicknameEdit.visibility = View.GONE
+            button.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
